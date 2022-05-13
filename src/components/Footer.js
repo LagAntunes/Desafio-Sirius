a56ae152198f952a5c44fd3fb8f5b7935022a800
LagAntunes <lag151617@gmail.com>
2022-05-13 18:35:14 -0300

    finalização do footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,24 @@
+import { FaLinkedin, FaGithub } from "react-icons/fa"
+
 import styles from "./Footer.module.css"
 
 function Footer() {
     return(
-        <footer>
-            <h1>footer</h1>
+        <footer className={styles.rodape}>
+            <h1>Projeto feito por <span>Luis Antonio</span> para a Sirius</h1>
+
+            <ul className={styles.icones}>
+                <li>
+                   <a href="https://www.linkedin.com/in/luis-antonio-garcia-antunes/">
+                        <FaLinkedin/>
+                   </a>
+                </li>
+                <li>
+                    <a href="https://github.com/LagAntunes">
+                        <FaGithub/>
+                    </a>
+                </li>
+            </ul>
         </footer>
     )
 }
