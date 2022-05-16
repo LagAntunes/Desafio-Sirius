7bce7781ec6570fc811ff1909f62d916cb448624
LagAntunes <lag151617@gmail.com>
2022-05-15 22:11:09 -0300

    finalização de montagem e estilização da página Sobre

--- src/pages/Sobre.js
+++ src/pages/Sobre.js
@@ -2,8 +2,24 @@ import styles from "./Sobre.module.css"
 
 function Sobre() {
     return(
-       <div>
-           <h1>sobre</h1>
+       <div className={styles.divContainer}>
+           <div className={styles.divItem}>
+            <h1>Informações sobre o trabalho:</h1>
+            <h3>
+                Trabalho realizado por Luis Antonio como proposta para a vaga de <span>desenvolvedor Front-End</span> na
+                Sirius Interativa.
+            </h3>
+           </div>
+
+           <div className={styles.divItem}>
+            <h1>Objetivos do trabalho:</h1>
+            <ul>
+                <li>Apresentar um projeto funcional dentro das <span>minhas habilidades</span></li>
+                <li>Aprender mais sobre o <span>consumo de APIs</span> em um projeto</li>
+                <li>Pesquisar e utilizar ferramentas do <span>React.Js</span></li>
+                <li>Conhecer e utilizar ferramentas como <span>Jasmine</span> e <span>Heroku</span></li>
+            </ul>
+           </div>
        </div>
     )
 }
